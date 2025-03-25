584f6decad40613b9a62663baf81df5c90c19398
Sattanathan2 <sattanathan2025@gmail.com>
2025-03-25 16:40:43 +0530

    first program
    create hello world

---
+++ python/first.py
@@ -0,0 +1,16 @@
+print("Hello World") #hello world  run fisrst line
+"""this python program"""
+
+name = input("Enter your name: ")
+print("Hello,", name, "! Welcome!")
+
+#single line variable
+s = "google"
+print(s)
+
+"""this one multi line variable"""
+
+t = " welocome"
+age = 25
+city = "chennai"
+print(t, age, city)
